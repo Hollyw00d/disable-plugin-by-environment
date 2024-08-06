71f5a7e6e80fd0cc41df015fdf26530e09bfee39
Matt Jennings <matt@mattjennings.net>
2024-08-06 13:58:34 -0700

    added plugin via wp scaffold

---
+++ disable-plugin-by-environment.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Disable Plugin by Environment
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Disable Plugin by Environment
+ * Author:          Matt Jennings
+ * Author URI:      https://www.mattjennings.net/
+ * Text Domain:     disable-plugin-by-environment
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Disable_Plugin_By_Environment
+ */
+
+// Your code starts here.
