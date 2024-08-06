cc39f14522bb48b0657712d05cb6faf4d85d8d44
Matt Jennings <matt@mattjennings.net>
2024-08-06 15:27:21 -0700

    created basic plugin for admin

--- disable-plugin-by-environment.php
+++ disable-plugin-by-environment.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:     Disable Plugin by Environment
- * Description:     Disable plugins by environment. For example, if you have a user tracking plugin that you want enabled only on the PROD environment website then it can disabled in other environment websites (STAGING, DEV, LOCAL. etc.).
+ * Description:     Disable plugins by environment. For example, if you have a user tracking plugin that you want enabled only on the PROD environment website then it can be disabled in other environment websites (STAGING, DEV, LOCAL, etc.).
  * Author:          Matt Jennings
  * Author URI:      https://www.mattjennings.net/
  * Text Domain:     disable-plugin-by-environment
@@ -12,5 +12,102 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly.
+    exit; // Exit if accessed directly.
 }
+
+// Global vars
+define('PLUGIN_NAME', 'Disable Plugin by Environment');
+define('PLUGIN_SLUG', 'disable-plugin-by-environment');
+
+class Disable_Plugin_By_Env {
+
+    public function init() {
+        add_action('admin_init', array($this, 'dpbe_register_settings'));
+        add_action('admin_menu', array($this, 'dpbe_settings_page'));
+    }
+
+    public function dpbe_register_settings() {
+        // Register the setting and the validation callback
+        register_setting(
+            'dpbe_example_plugin_options',    // Option group
+            'dpbe_example_plugin_options',    // Option name in the database
+            array($this, 'dpbe_example_plugin_options_validate') // Validation callback
+        );
+
+        // Add the settings section
+        add_settings_section(
+            'api_settings',                    // Section ID
+            'API Settings',                    // Title of the section
+            array($this, 'dpbe_plugin_section_text'),  // Callback to output the description
+            'dpbe_example_plugin'               // Page on which the section appears
+        );
+
+        // Add the settings field
+        add_settings_field(
+            'dpbe_plugin_setting_api_key',      // Field ID
+            'API Key',                         // Field title
+            array($this, 'dpbe_plugin_setting_api_key'),  // Callback to output the form field
+            'dpbe_example_plugin',              // Page on which the field appears
+            'api_settings'                     // Section in which the field appears
+        );
+    }
+
+    public function dpbe_settings_page() {
+        add_options_page(
+            PLUGIN_NAME,
+            PLUGIN_NAME,
+            'manage_options',
+            PLUGIN_SLUG,
+            array($this, 'dpbe_settings_page_html')
+        );
+    }
+
+    public function dpbe_settings_page_html() {
+        if (!current_user_can('manage_options')) return;
+
+        $options = get_option('dpbe_example_plugin_options');
+
+        print_r($options);
+        ?>
+        <div class="wrap">
+
+
+            <h2><?php echo PLUGIN_NAME; ?></h2>
+            <form action="options.php" method="post">
+                <?php 
+                settings_fields('dpbe_example_plugin_options');
+                do_settings_sections('dpbe_example_plugin');
+                ?>
+                <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e('Save'); ?>" />
+            </form>
+        </div>
+        <?php
+    }
+
+    public function dpbe_plugin_section_text() {
+        echo '<p>Here you can set all the options for using the API.</p>';
+    }
+
+    public function dpbe_plugin_setting_api_key() {
+        $options = get_option('dpbe_example_plugin_options');
+        $api_key = isset($options['api_key']) ? $options['api_key'] : '';
+        echo "<input id='dpbe_plugin_setting_api_key' name='dpbe_example_plugin_options[api_key]' type='text' value='" . esc_attr($api_key) . "' />";
+    }
+
+    public function dpbe_example_plugin_options_validate($input) {
+        $newinput = array();
+        $newinput['api_key'] = trim($input['api_key']);
+
+        // Validate the API key (for example, a 32-character alphanumeric string)
+        if (!preg_match('/^[a-z0-9]{4}$/i', $newinput['api_key'])) {
+            $newinput['api_key'] = ''; // Clear the input if it's not valid
+        }
+
+        return $newinput;
+    }
+}
+
+$disable_plugin_by_env = new Disable_Plugin_By_Env();
+$disable_plugin_by_env->init();
+
+?>
