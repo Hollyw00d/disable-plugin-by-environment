24b9b0991e1ec3ae6d7965715455977b56334357
Matt Jennings <matt@mattjennings.net>
2024-08-06 14:06:23 -0700

    added basic info to plugin file

--- disable-plugin-by-environment.php
+++ disable-plugin-by-environment.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * Plugin Name:     Disable Plugin by Environment
- * Plugin URI:      PLUGIN SITE HERE
- * Description:     Disable Plugin by Environment
+ * Description:     Disable plugins by environment. For example, if you have a user tracking plugin that you want enabled only on the PROD environment website then it can disabled in other environment websites (STAGING, DEV, LOCAL. etc.).
  * Author:          Matt Jennings
  * Author URI:      https://www.mattjennings.net/
  * Text Domain:     disable-plugin-by-environment
@@ -12,4 +11,6 @@
  * @package         Disable_Plugin_By_Environment
  */
 
-// Your code starts here.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
